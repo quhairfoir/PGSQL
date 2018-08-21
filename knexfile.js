af4faf425bc54d08d3044bed3ff7af1cc7380fc9
Morag St. Clair <moragstc@gmail.com>
2018-08-21 18:54:26 +0000

    knex migration files added

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+const settings = require('./settings');
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
